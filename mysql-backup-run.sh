8829b052bec8d38ad97b24132f1d0903700f0e6d
Dave Lee <dave@davelee.de>
2016-12-29 08:25:54 +0000

    use command line params for mysql and backup paths

--- mysql-backup-run.sh
+++ mysql-backup-run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#Check that input parameters (/path/to/mysql and /path/to/backup) are present. Otherwise return usage message and exit.
+if [ $# -ne 2 ]
+  then
+    echo "Usage: mysql-backup-run.sh /path/to/mysql /path/to/backup"
+    exit
+fi
 #Set the name of the file containing the backup - a backup on the 21st December 2016 at 21:00 would have the following name - backup.201612212100.sql
 BACKUPFILE=backup.`date +"%Y%m%d%H%M"`.sql
-# Change /path/to/mysql to the root folder of your mysql installation and /path/to/backup to the directory where backups should be stored.
-/path/to/mysql/bin/mysqldump -uroot --all-databases > /path/to/backup/${BACKUPFILE}
+# Call backup method in mysql and redirect output to backup file.
+${1}/bin/mysqldump -uroot --all-databases > ${2}/${BACKUPFILE}
